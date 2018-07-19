5e3613ea140ad844a35d41d4cf87737b5b260a31
Antti J Manninen <antti.j.manninen@helsinki.fi>
2018-07-19 11:26:32 +0300

    Removed duplicate files.

--- halo_toolbox/write_nc_struct.m
+++
@@ -1,4 +0,0 @@
-function write_nc_struct(pathin, dim, data, att)
-write_nc_silent(pathin, dim, data, att)
-end
-
--- halo_toolbox/zero2one.m
+++
@@ -1,9 +0,0 @@
-function [ x_norm ] = zero2one(x)
-%ZERO2ONE normalizes the values of the given array between 0 and 1
-%
-% 2015-11-24
-% Antti Manninen
-% antti.j.manninen@helsinki.fi
-x_norm = (x - nanmin(x(:))) ./ (nanmax(x(:)) - nanmin(x(:)));
-end
-
