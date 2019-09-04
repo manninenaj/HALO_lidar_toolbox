b96e5f0bef5f99d3b8789055fd6db7612ff88e2c
Antti J Manninen <antti.manninen@fmi.fi>
2019-09-04 11:37:48 +0100

    Added a leapyear check to dayofyear.m

--- general_utilities/dayofyear.m
+++ general_utilities/dayofyear.m
@@ -10,7 +10,8 @@ function yd = dayofyear(varargin)
 %
 %   If no date is specified, the current date and time is used.  Gregorian
 %   calendar is assumed.
-
+%
+%   --- This function is modifed from ---
 %   Author:      Peter John Acklam
 %   Time-stamp:  2002-03-03 12:52:04 +0100
 %   E-mail:      pjacklam@online.no
@@ -28,8 +29,10 @@ function yd = dayofyear(varargin)
 
    days_in_prev_months = [0 31 59 90 120 151 181 212 243 273 304 334];
 
+   isleapyear = mod(year,4)==0 & (mod(year,100)~=0 | mod(year,400)==0);
+   
    % Day in given month.
    yd = days_in_prev_months(month) ...               % days in prev. months
-        + ( isleapyear(year) & ( month > 2 ) ) ...   % leap day
+        + ( isleapyear & ( month > 2 ) ) ...   % leap day
         + day ...                                    % day in month
         + ( second + 60*minute + 3600*hour )/86400;  % part of day
