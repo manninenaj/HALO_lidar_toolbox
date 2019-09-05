aa8942571e048d4a00185135ec595d6dd900af91
Antti J Manninen <antti.manninen@fmi.fi>
2019-09-05 18:10:17 +0100

    Added mmpolar and pcolor2polar functions.

---
+++ general_utilities/pcolor2polar.m
@@ -0,0 +1,23 @@
+function [xp,yp,zp,thpt,rpt] = pcolor2polar(time,range,azimuth,data)
+
+  % Original data (in cartesian space)
+  x = time;
+  y = range;
+  z = data;
+
+  % Convert to polar
+  r = range;
+  th = azimuth;
+  xp = r.*cosd(th);
+  yp = r.*sind(th);
+  zp = interp2(x,y,z,xp,yp);
+  [thpt, rpt] = cart2pol(xpt, ypt);
+  
+  %% Plot in polar
+  %ax(2) = subplot(1,2,2);
+  %pcolor(xp,yp,zp);
+  %shading flat;
+  %axis equal;
+  %ax(3) = axes('position', get(ax(2), 'position'));
+  %hp = mmpolar(thpt, rpt, 'ko', 'RLimit', [0 50], 'backgroundcolor', 'none');
+  %set(ax(2), 'visible', 'off');
